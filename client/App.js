097a3dccfc22f00b3bcb12f94811fa60af374f2e
seanspiesman <57199893+seanspiesman@users.noreply.github.com>
2020-03-12 16:12:48 -0500

    heart toggle working

--- client/App.js
+++ client/App.js
@@ -73,7 +73,14 @@ export default class App extends React.Component {
     element.classList.add("selectedImage");
   }
 
-  heartClick() {}
+  heartClick() {
+    if (document.getElementsByClassName("redHeart").length !== 0) {
+      document.getElementById("heart").classList.remove("redHeart");
+    } else {
+      let element = document.getElementById("heart");
+      element.classList.add("redHeart");
+    }
+  }
 
   render() {
     return (
@@ -103,12 +110,14 @@ export default class App extends React.Component {
             </div>
             <div className="buttonColumn2">
               <svg
+                onClick={this.heartClick.bind(this)}
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 50 25"
                 aria-hidden="true"
                 focusable="false"
               >
                 <path
+                  id="heart"
                   className="heart"
                   d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z"
                 ></path>
