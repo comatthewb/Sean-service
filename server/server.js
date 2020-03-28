3d427ec5f82b43497bc0d2eb79e4e65f35e206d3
seanspiesman <57199893+seanspiesman@users.noreply.github.com>
2020-03-28 16:41:10 -0500

    added control requests

--- server/server.js
+++ server/server.js
@@ -6,7 +6,15 @@ const cors = require("cors");
 const data = require("../datatoinsert.json");
 const { db, getItemImages, insertItems } = require("./db");
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../dist")));
